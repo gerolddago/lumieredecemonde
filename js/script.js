1abb8460cd8f42f0ccf5f4085633053d5dc27981
gerolddago <115063769+gerolddago@users.noreply.github.com>
2024-09-21 02:22:18 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// const blurDivs = document.querySelectorAll(".blur-load");
+
+// blurDivs.forEach(div => {
+//     const img = div.querySelector("img");
+
+//     function loaded() {
+//         div.classList.add("loaded");
+//     }
+
+//     if (img.complete) {
+//         loaded();
+//     } else {
+//         img.addEventListener("load", loaded);
+//     }
+// })
+
+
+
+
+
+
+// const img = document.querySelectorAll("img");
+
+
+// if (img.complete) {
+//     div.classList.add("loaded");
+//     img.style.opacity = "1";
+//     console.log('img loaded')
+
+// } else if (!img.complete) {
+//     img.style.opacity = "0";
+//     console.log('img loading');
+// }
